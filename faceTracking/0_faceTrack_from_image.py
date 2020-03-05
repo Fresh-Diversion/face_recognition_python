42bd2147401348753bacca925fdb64a09194ecde
FreshDiversion <freshdiversion@gmail.com>
2020-03-05 16:11:22 +0900

    Add files via upload

---
+++ faceTracking/0_faceTrack_from_image.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 10:38:02 2020
+
+@author: todd
+"""
+
+import cv2 as cv
+
+# Read image from your local file system
+original_image = cv.imread('images/square-face.jpg')
+
+# Convert color image to grayscale for Viola-Jones
+grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+# Load the classifier and create a cascade object for face detection
+face_cascade = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+detected_faces = face_cascade.detectMultiScale(grayscale_image)
+
+for (column, row, width, height) in detected_faces:
+    cv.rectangle(
+        original_image,
+        (column, row),
+        (column + width, row + height),
+        (0, 255, 0),
+        2
+    )
+
+#throws an error
+cv.imshow('Image', original_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
